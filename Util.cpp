a8c97ed34cded6b8eb3314bd404ced9d7f944ac0
suzker <zsu2@buffalo.edu>
2012-11-08 14:42:19 -0500

    [mod]static member/const var in Util

--- Util.cpp
+++ Util.cpp
@@ -1,23 +1,14 @@
 #include "Util.h"
 
-Util::Util(){
-   Util::__init_arg();
-}
-
-void Util::__init_arg(){
-    Util::arg_print_usage = 0;
-    Util::arg_available_frame = 5;
-    Util::arg_replace_policy = RPLCY_FIFO;
-    Util::arg_input_filepath = "N/A";
-}
+int Util::arg_print_usage = 0;
+int Util::arg_available_frame = 5;
+Util::replace_policy_t Util::arg_replace_policy = BELADY;
+string Util::arg_input_filepath = "N/A";
 
 const int Util::parse(int argc, char * argv[]){
     int i, err = 0;
     for (i=1; i<argc; ++i){
-        if (err == 1) {
-            err = 1;
-            break;
-        }
+        if (err) {break;}
         if (argv[i][0] != '-'){
             err = 1;
             break;
@@ -41,42 +32,42 @@ const int Util::parse(int argc, char * argv[]){
                     if (++i < argc) {
                         string _tmp_ = argv[i];
                         if(_tmp_ == "FIFO") {
-                            Util::arg_replace_policy = RPLCY_FIFO;
+                            Util::arg_replace_policy = FIFO;
                             break;
                         }
                         if(_tmp_ == "LFU") {
-                            Util::arg_replace_policy = RPLCY_LFU;
+                            Util::arg_replace_policy = LFU;
                             break;
                         }
                         if(_tmp_ == "LRU_STACK") {
-                            Util::arg_replace_policy = RPLCY_LRU_STACK;
+                            Util::arg_replace_policy = LRU_STACK;
                             break;
                         }
                         if(_tmp_ == "LRU_CLOCK") {
-                            Util::arg_replace_policy = RPLCY_LRU_CLOCK;
+                            Util::arg_replace_policy = LRU_CLOCK;
                             break;
                         }
                         if(_tmp_ == "LRU_REF8") {
-                            Util::arg_replace_policy = RPLCY_LRU_REF8;
+                            Util::arg_replace_policy = LRU_REF8;
                             break;
                         }
                         if(_tmp_ == "OPTIMAL") {
-                            Util::arg_replace_policy = RPLCY_BELADY;
+                            Util::arg_replace_policy = BELADY;
                             break;
                         }
                     } else {
                         err = 1;
+                        break;
                     }
-                    break;
                 default:
                     err = 1;
-                    break;
             }
         }
-        if (err == 1) {
-            cerr << "[ERR] incorrect arguments." << endl;
-            return 0;
-        }
-        return 1;
     }
+    if (err == 1) {
+        cout << "[ERR] incorrect arguments." << endl;
+        return 0;
+    }
+    cout << "usage: " << Util::arg_print_usage << ", frames: " << Util::arg_available_frame << ", filepath: " << Util::arg_input_filepath << endl;
+    return 1;
 }
--- Util.h
+++ Util.h
@@ -6,26 +6,18 @@
 #define __HEADER_UTILITY__
 #include <iostream>
 #include <string>
-#include <string.h> //clib for string
 
 using namespace std;
 
 class Util {
     public:
+        enum replace_policy_t {FIFO, LFU, LRU_STACK, LRU_CLOCK, LRU_REF8, BELADY};
         static int arg_print_usage;
         static int arg_available_frame;
-        static int arg_replace_policy;
+        static replace_policy_t arg_replace_policy;
         static string arg_input_filepath;
-        Util();
+        const int parse(int, char **);
+        Util(){};
         ~Util();
-        const int parse(int argc, char * argv[]);
-    private:
-        static const int RPLCY_FIFO = 1;
-        static const int RPLCY_LFU = 2;
-        static const int RPLCY_LRU_STACK = 3;
-        static const int RPLCY_LRU_CLOCK = 4;
-        static const int RPLCY_LRU_REF8 = 5;
-        static const int RPLCY_BELADY = 6;
-        void __init_arg();
 };
 #endif
