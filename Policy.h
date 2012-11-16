12595e0855f11fe370cd02b34f2a45e05b8dd4df
suzker <zsu@buffalo.edu>
2012-11-16 17:43:23 -0500

    [fix] Policy

--- Policy.h
+++ Policy.h
@@ -5,42 +5,34 @@
 #define __HEADER_POLICY__
 #include <iostream>
 #include <list>
+#include <set>
 #define DEFAULT_FRAME_SIZE 5;
 
 using namespace std;
-template<typename T>
+template<class T>
 class Policy{
     public:
-        Policy(){
-            Policy(DEFAULT_FRAME_SIZE);
-        }
-        Policy(int size){
-            _frames = new list<T>(size);
-            _pages_loaded = new set<T>(size);
-        }
-        virtual size_t getNextFrameIdx() const;
-        virtual bool replaceFrame(size_t idx, T data) const{
-            if (idx >= _frames->size()){return false;}
-            typename list<T>::iterator _itr = _frames->begin();
-            _itr+=idx;
-            _pages_loaded->erase(_pages_loaded->find(*_itr));
-            typename list<T>::iterator _itr_back = _frames->erase(_itr);
-            _frames->insert(_itr_back, data);
-            _pages_loaded->insert(data);
-            return true;
+        Policy(size_t num_frames){
+            _num_frames = num_frames;
+            __frames = new list<T>();
+            __pages_loaded = new set<T>();
+            __num_page_fault = 0;
         }
         ~Policy();
+        virtual bool addPage(T) const;
+        int getNumPageFault() const;
+        void printFrames();
+
     private:
-        size_t _num_frames;
-        list<T> * _frames;
-        set<T> * _pages_loaded;
+        list<T> * __frames;
+        set<T> * __pages_loaded;
+        int __num_page_fault;
+
     protected:
-        bool _isPageFaultExist(T data) const{
-            if (_pages_loaded->find(data) != _pages_loaded->end()){
-                return true;
-            }
-            return false;
-        }
-}
+        size_t _num_frames;
+        virtual size_t _getNextFrameIdx() const;
+        virtual bool _replaceFrame(size_t, T) const;
+        bool _testPageFault(T) const;
+};
 
 #endif
